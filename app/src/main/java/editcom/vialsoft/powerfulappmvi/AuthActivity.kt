49a9af823170e111cf4d7469bc5b05667d1cc8ed
Alvaro Isea <alvaroe9448@gmail.com>
2020-12-09 20:06:34 +0100

    initial setup done

---
+++ app/src/main/java/editcom/vialsoft/powerfulappmvi/AuthActivity.kt
@@ -0,0 +1,11 @@
+package editcom.vialsoft.powerfulappmvi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
